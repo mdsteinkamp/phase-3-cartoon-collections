0657740aff5aa8c55ac961c8fc316a1bd36ae8bd
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-05-03 21:42:50 -0700

    commit tree 457e336eae0014aedf9a1c3432dec9638c0a94a7

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet calls
+  all_calls = calls.map do |call|
+    call.capitalize + "!"
+  end
+  all_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.count {|call| call.length > 4} > 0 ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese snacks
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |snack|
+    cheese_types.include?(snack) ? "#{snack}" : nil
+  end
+
 end
+
+# roll_call_dwarves ["sleepy", "grumpy", "dopey", "bashful"]
+# summon_captain_planet ["fire", "water", "air"]
+# long_planeteer_calls ["two", "go", "industrious", "bop"]
+puts find_the_cheese ["crackers", "gouda", "thyme"]
